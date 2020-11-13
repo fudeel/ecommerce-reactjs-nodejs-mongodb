357b05d0f5f94f105c6a9697066538af2a599901
FAMUCA <fadil.muca@capgemini.com>
2020-11-13 12:17:37 +0100

    user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,6 +32,41 @@ const authUser = asyncHandler(async (req, res) => {
 
 
 
+// @desc   Register new user
+// @route  POST /api/user
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    console.log('req: ', req)
+
+    // find one document and we find by email
+    const userExist = await User.findOne({ email })
+
+    if (userExist) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = User.create({
+        name, email, password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+
 // @desc   Get user profile
 // @route  GET /api/user/profile
 // @access Private
@@ -56,4 +91,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
